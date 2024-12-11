5e36894b49575e565965db682fb5daf86551c0c7
segev.raz <welegionsr@gmail.com>
2024-12-11 16:49:55 +0200

    updated config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack(config, { isServer }) {
+        if (!isServer) {
+            config.watchOptions = {
+                ignored: /node_modules/,
+                poll: 1000, // Optional: Adjust polling interval
+            };
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
