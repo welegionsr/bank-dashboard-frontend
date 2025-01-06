3b4bf44c5074c0d375dfe564d1114173cbbdc50f
segev.raz <welegionsr@gmail.com>
2025-01-06 11:58:23 +0200

    added /verify to the middleware checks, so that logged in users would be redirected from it

--- src/middleware.js
+++ src/middleware.js
@@ -6,14 +6,14 @@ export async function middleware(req) {
     const isValid = req.cookies.get('session_valid')?.value === 'true';
     const userRole = req.cookies.get('session_role')?.value;
 
-    console.log("[Middleware] ", "session_valid: ", req.cookies.get('session_valid'));
-    console.log("[Middleware] ", "IsValid: ", isValid);
-    console.log("[Middleware] ", "UserRole: ", userRole);
+    console.log("[Middleware]", "session_valid: ", req.cookies.get('session_valid'));
+    console.log("[Middleware]", "IsValid: ", isValid);
+    console.log("[Middleware]", "UserRole: ", userRole);
 
 
-    if (url.pathname === '/login' || url.pathname === '/register') {
+    if (url.pathname === '/login' || url.pathname === '/register' || url.pathname === '/verify') {
         if (isValid) {
-            console.log("[Middleware] ", "Session valid. Redirecting to /dashboard.");
+            console.log("[Middleware]", "Session valid. Redirecting to /dashboard.");
             return NextResponse.redirect(new URL('/dashboard', req.url));
         }
         return NextResponse.next();
@@ -23,41 +23,41 @@ export async function middleware(req) {
 
         // Check if accessing an admin route and if the user has the 'admin' role
         if (url.pathname.startsWith('/admin') && userRole !== 'admin') {
-            console.log("[Middleware] ", "User does not have admin privileges. Redirecting to /dashboard.");
+            console.log("[Middleware]", "User does not have admin privileges. Redirecting to /dashboard.");
             return NextResponse.redirect(new URL('/dashboard', req.url));
         }
 
-        console.log("[Middleware] ", "Session valid. Allowing access.");
+        console.log("[Middleware]", "Session valid. Allowing access.");
         return NextResponse.next();
     }
 
     try {
-        console.log("[Middleware] ", "Checking session validity with the server...");
+        console.log("[Middleware]", "Checking session validity with the server...");
         const response = await apiClient.get('/auth/session', {
             headers: { 'Cookie': req.headers.get('cookie') },
         });
 
         if (response.status === 200 && response.data.isValid) {
-            console.log("[Middleware] ", "Session valid. Setting validation cookie.");
+            console.log("[Middleware]", "Session valid. Setting validation cookie.");
             const res = NextResponse.next();
             res.cookies.set('session_valid', 'true', { maxAge: 300, httpOnly: true });
             res.cookies.set('session_role', response.data.role, { maxAge: 300, httpOnly: true });
             
             // Check if accessing an admin route and if the user has the 'admin' role
             if (url.pathname.startsWith('/admin') && response.data.role !== 'admin') {
-                console.log("[Middleware] ", "User does not have admin privileges. Redirecting to /dashboard.");
+                console.log("[Middleware]", "User does not have admin privileges. Redirecting to /dashboard.");
                 return NextResponse.redirect(new URL('/dashboard', req.url));
             }
             
             return res;
         }
     } catch (error) {
-        console.log("[Middleware] ", "Session invalid. Redirecting to /login.");
+        console.log("[Middleware]", "Session invalid. Redirecting to /login.");
     }
 
     return NextResponse.redirect(new URL('/login', req.url));
 }
 
 export const config = {
-    matcher: ['/login', '/register', '/admin/:path*', '/dashboard/:path*'],
+    matcher: ['/login', '/register', '/verify', '/admin/:path*', '/dashboard/:path*'],
 };
